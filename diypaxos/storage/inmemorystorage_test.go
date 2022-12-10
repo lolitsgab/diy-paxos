70d9fd332678f12c54075be1dd5a64d635705214
Gabriel Garcia <gabrielgarc23@gmail.com>
2022-12-10 13:00:25 -0800

    Migrate to sync map (#8)

--- diypaxos/storage/inmemorystorage_test.go
+++ diypaxos/storage/inmemorystorage_test.go
@@ -1,13 +1,33 @@
 package storage
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 type FakeKvStoreServer struct {
 	InMemoryStorage
 }
 
-func (s *FakeKvStoreServer) setStore(store map[string]Value) {
+func (s *FakeKvStoreServer) setStore(store *sync.Map) {
 	s.store = store
+}
+
+func toSyncStore(m map[string]Value) *sync.Map {
+	sm := &sync.Map{}
+	for k, v := range m {
+		sm.Store(k, v)
+	}
+	return sm
+}
+
+func getSize(m *sync.Map) int {
+	length := 0
+	m.Range(func(_, _ interface{}) bool {
+		length++
+		return true
+	})
+	return length
 }
 
 func TestInsert(t *testing.T) {
@@ -15,19 +35,19 @@ func TestInsert(t *testing.T) {
 		name    string
 		key     string
 		val     int
-		store   map[string]Value
+		store   *sync.Map
 		wantErr bool
 	}{
 		{
 			name:  "Insert new",
 			key:   "new",
 			val:   1,
-			store: map[string]Value{},
+			store: toSyncStore(map[string]Value{}),
 		}, {
 			name:    "Insert existing",
 			key:     "exist",
 			val:     1,
-			store:   map[string]Value{"exist": Value{Value: 1}},
+			store:   toSyncStore(map[string]Value{"exist": Value{Value: 1}}),
 			wantErr: true,
 		},
 	} {
@@ -46,19 +66,19 @@ func TestGet(t *testing.T) {
 		name    string
 		key     string
 		val     int
-		store   map[string]Value
+		store   *sync.Map
 		wantErr bool
 	}{
 		{
 			name:    "Get DNE",
 			key:     "new",
 			wantErr: true,
-			store:   map[string]Value{},
+			store:   toSyncStore(map[string]Value{}),
 		}, {
 			name:  "Get Existing",
 			key:   "exist",
 			val:   1,
-			store: map[string]Value{"exist": Value{Value: 1}},
+			store: toSyncStore(map[string]Value{"exist": Value{Value: 1}}),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -77,31 +97,31 @@ func TestRemove(t *testing.T) {
 	for _, tc := range []struct {
 		name    string
 		key     string
-		store   map[string]Value
+		store   *sync.Map
 		wantErr bool
 	}{
 		{
 			name:    "Remove DNE",
 			key:     "dne",
-			store:   map[string]Value{},
+			store:   toSyncStore(map[string]Value{}),
 			wantErr: true,
 		}, {
 			name:  "Remove Existing",
 			key:   "exist",
-			store: map[string]Value{"exist": Value{Value: 1}},
+			store: toSyncStore(map[string]Value{"exist": Value{Value: 1}}),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			storage := &FakeKvStoreServer{}
 			storage.setStore(tc.store)
-			oldSize := len(tc.store)
+			oldSize := getSize(tc.store)
 			if err := storage.Remove(tc.key); (err != nil && !tc.wantErr) || (err == nil && tc.wantErr) {
 				t.Errorf("got error %v; wanted err? %v", err.Error(), tc.wantErr)
 			}
 			if tc.wantErr {
 				return
 			}
-			if want, got := oldSize-1, len(tc.store); want != got {
+			if want, got := oldSize-1, getSize(tc.store); want != got {
 				t.Errorf("len(store)=%v, wanted %v", want, got)
 			}
 		})
@@ -113,19 +133,19 @@ func TestUpdate(t *testing.T) {
 		name    string
 		key     string
 		new     int
-		store   map[string]Value
+		store   *sync.Map
 		wantErr bool
 	}{
 		{
 			name:    "Update DNE",
 			key:     "dne",
-			store:   map[string]Value{},
+			store:   toSyncStore(map[string]Value{}),
 			wantErr: true,
 		}, {
 			name:  "Update Existing",
 			key:   "exist",
 			new:   2,
-			store: map[string]Value{"exist": Value{Value: 1}},
+			store: toSyncStore(map[string]Value{"exist": Value{Value: 1}}),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -137,7 +157,7 @@ func TestUpdate(t *testing.T) {
 			if tc.wantErr {
 				return
 			}
-			if val, _ := tc.store[tc.key]; val.Value != int32(tc.new) {
+			if val, _ := tc.store.Load(tc.key); val.(Value).Value != int32(tc.new) {
 				t.Errorf("got %v, want %v", val, tc.new)
 			}
 		})
@@ -149,18 +169,18 @@ func TestUpsert(t *testing.T) {
 		name    string
 		key     string
 		new     int
-		store   map[string]Value
+		store   *sync.Map
 		wantErr bool
 	}{
 		{
 			name:  "Update DNE",
 			key:   "dne",
-			store: map[string]Value{},
+			store: toSyncStore(map[string]Value{}),
 		}, {
 			name:  "Update Existing",
 			key:   "exist",
 			new:   2,
-			store: map[string]Value{"exist": {Value: 1}},
+			store: toSyncStore(map[string]Value{"exist": {Value: 1}}),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -172,7 +192,7 @@ func TestUpsert(t *testing.T) {
 			if tc.wantErr {
 				return
 			}
-			if val, _ := tc.store[tc.key]; val.Value != int32(tc.new) {
+			if val, _ := tc.store.Load(tc.key); val.(Value).Value != int32(tc.new) {
 				t.Errorf("got %v, want %v", val, tc.new)
 			}
 		})
