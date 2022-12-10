d6c9f284c4d6678b0180bced0b1bdb408db92ee8
Gabriel Raphael Garcia Montoya <gabrielgarc23@gmail.com>
2022-12-10 12:59:47 -0800

    Migrate to sync map

--- diypaxos/storage/inmemorystorage.go
+++ diypaxos/storage/inmemorystorage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"sync"
 )
 
 type Metadata struct {
@@ -25,52 +26,53 @@ type Storage interface {
 
 // InMemoryStorage implements Storage with an in-memory table.
 type InMemoryStorage struct {
-	store map[string]Value
+	store *sync.Map
 }
 
 // NewInMemoryStorage creates a new InMemoryStorage instance.
 func NewInMemoryStorage() Storage {
-	return &InMemoryStorage{map[string]Value{}}
+	return &InMemoryStorage{&sync.Map{}}
 }
 
 // Insert inserts a KV pair into the Storage container.
 func (i *InMemoryStorage) Insert(k string, v int32) error {
-	if _, exists := i.store[k]; exists {
+	if _, exists := i.store.Load(k); exists {
 		return status.New(codes.AlreadyExists, "cannot insert to an already populated key").Err()
 	}
-	i.store[k] = Value{Value: v}
+	i.store.Store(k, Value{Value: v})
 	return nil
 }
 
 // Get fetches value by key from the Storage container.
 func (i *InMemoryStorage) Get(k string) (int32, error) {
-	if _, exists := i.store[k]; !exists {
+	got, exists := i.store.Load(k)
+	if !exists {
 		return -1, status.New(codes.NotFound, "cannot get non existent key").Err()
 	}
-	return i.store[k].Value, nil
+	return got.(Value).Value, nil
 }
 
 // Remove deletes a value by key from a storage container.
 func (i *InMemoryStorage) Remove(k string) error {
-	if _, exists := i.store[k]; !exists {
+	if _, exists := i.store.Load(k); !exists {
 		return status.New(codes.NotFound, "cannot remove non existent key").Err()
 	}
-	delete(i.store, k)
+	i.store.Delete(k)
 	return nil
 }
 
 // Update changes the value associated with a key entry in the Storage container iff it exists.
 func (i *InMemoryStorage) Update(k string, v int32) error {
-	if _, exists := i.store[k]; !exists {
+	if _, exists := i.store.Load(k); !exists {
 		return status.New(codes.NotFound, "cannot update a non existent key").Err()
 	}
-	i.store[k] = Value{Value: v}
+	i.store.Store(k, Value{Value: v})
 	return nil
 }
 
 // Upsert changes the value associated with a key entry in the Storage container, or creates it if it does not exist.
 func (i *InMemoryStorage) Upsert(k string, v int32) error {
-	if _, exists := i.store[k]; !exists {
+	if _, exists := i.store.Load(k); !exists {
 		return i.Insert(k, v)
 	}
 	return i.Update(k, v)
